001150b8d9de7bed54043a9aef7786092ea69495
Ben <benmiller1315@gmail.com>
2020-02-11 19:43:35 +0100

    in progress seperating out the stages

--- qm9_train.py
+++ qm9_train.py
@@ -16,123 +16,122 @@ from e3nn.non_linearities import rescaled_act
 from arguments import train_parser, qm9_property_selector
 from networks import create_kernel, Network, OutputScalarNetwork, ResNetwork
 
-# Setup script
-torch.set_default_dtype(torch.float32)
-logging.basicConfig(level=os.environ.get("LOGLEVEL", "INFO"))
-parser = argparse.ArgumentParser(parents=[train_parser(), qm9_property_selector()])
-args = parser.parse_args()
-properties = [vars(QM9)[k] for k, v in vars(args).items() if k in QM9.properties and v]
-if not properties:
-    raise ValueError("No properties were selected to train on.")
-device = torch.device("cpu") if args.cpu else torch.device("cuda")
 
-# Build directory
-try:
-    os.makedirs(args.model_dir)
-except FileExistsError:
-    logging.warning(f"Model directory {args.model_dir} exists.")
-    if args.overwrite:
-        logging.warning("Overwriting.")
-        rmtree(args.model_dir)
-        os.makedirs(args.model_dir)
+def configuration(args):
+    torch.set_default_dtype(torch.float32)
+    logging.basicConfig(level=os.environ.get("LOGLEVEL", "INFO"))
+    if torch.cuda.is_available() and not args.cpu:
+        device = torch.device("cuda")
     else:
-        logging.warning("Loading from checkpoint.")
+        device = torch.device("cpu")
+    return device
 
-torch.save(args, args.model_dir + "args.pkl")
 
-# data preparation
-logging.info("get dataset")
-split_file = os.path.join(args.model_dir, "split.npz") if not args.split_file else args.split_file
-dataset = QM9(args.db, load_only=properties)
-train, val, test = spk.train_test_split(
-    dataset,
-    num_train=args.ntr,
-    num_val=args.nva,
-    split_file=split_file
-)
-assert len(train) == args.ntr
-assert len(val) == args.nva
-train_loader = spk.AtomsLoader(train, batch_size=args.bs, shuffle=True, num_workers=args.num_workers)
-val_loader = spk.AtomsLoader(val, batch_size=args.bs, num_workers=args.num_workers)
+def create_or_load_directory(args):
+    try:
+        os.makedirs(args.model_dir)
+        torch.save(args, args.model_dir + "args.pkl")
+    except FileExistsError:
+        logging.warning(f"Model directory {args.model_dir} exists.")
+        if args.overwrite:
+            logging.warning("Overwriting.")
+            rmtree(args.model_dir)
+            os.makedirs(args.model_dir)
+            torch.save(args, args.model_dir + "args.pkl")
+        else:
+            logging.warning("Loading from checkpoint, continuing using SAVED args.")
+            args = torch.load(args.model_dir + "args.pkl")
+    return args
 
-# statistics
-atomrefs = dataset.get_atomref(properties)
-split_npz = np.load(split_file)
-try:
-    logging.info(f"statistics loaded from {split_file}")
-    means = {prop: torch.from_numpy(split_npz[f'{prop}_mean']) for prop in properties}
-    stddevs = {prop: torch.from_numpy(split_npz[f'{prop}_stddev']) for prop in properties}
-except KeyError:
-    means, stddevs = train_loader.get_statistics(
-        properties, divide_by_atoms=True, single_atom_ref=atomrefs
-    )  # TODO, the means are used for the validation data as well which seems wrong to me.
-    np.savez(
-        split_file,
-        **{f'{prop}_mean': means[prop] for prop in properties},
-        **{f'{prop}_stddev': stddevs[prop] for prop in properties},
-        **split_npz
+
+def get_data(args, properties):
+    split_file = os.path.join(args.model_dir, "split.npz") if not args.split_file else args.split_file
+    dataset = QM9(args.db, load_only=properties)
+    train, val, test = spk.train_test_split(
+        dataset,
+        num_train=args.ntr,
+        num_val=args.nva,
+        split_file=split_file
+    )
+    assert len(train) == args.ntr
+    assert len(val) == args.nva
+    train_loader = spk.AtomsLoader(train, batch_size=args.bs, shuffle=True, num_workers=args.num_workers)
+    val_loader = spk.AtomsLoader(val, batch_size=args.bs, num_workers=args.num_workers)
+    test_loader = spk.AtomsLoader(test, batch_size=args.bs, num_workers=args.num_workers)
+    return dataset, split_file, train_loader, val_loader, test_loader
+
+
+def get_statistics(dataset, split_file, properties, train_loader):
+    atomrefs = dataset.get_atomref(properties)
+    split_npz = np.load(split_file)
+    try:
+        means = {prop: torch.from_numpy(split_npz[f'{prop}_mean']) for prop in properties}
+        stddevs = {prop: torch.from_numpy(split_npz[f'{prop}_stddev']) for prop in properties}
+        logging.info(f"statistics loaded from {split_file}")
+    except KeyError:
+        means, stddevs = train_loader.get_statistics(
+            properties, divide_by_atoms=True, single_atom_ref=atomrefs
+        )  # TODO, the means are used for the validation data as well which seems wrong to me.
+        np.savez(
+            split_file,
+            **{f'{prop}_mean': means[prop] for prop in properties},
+            **{f'{prop}_stddev': stddevs[prop] for prop in properties},
+            **split_npz
+        )
+        logging.info(f"statistics saved to {split_file}")
+    return atomrefs, means, stddevs
+
+
+def create_model(args, atomrefs, means, stddevs, properties):
+    ssp = rescaled_act.ShiftedSoftplus(beta=args.beta)
+    kernel = create_kernel(
+        cutoff=args.rad_maxr,
+        n_bases=args.rad_nb,
+        n_neurons=args.rad_h,
+        n_layers=args.rad_L,
+        act=ssp
     )
 
-# model build
-logging.info("build model")
+    sp = rescaled_act.Softplus(beta=args.beta)
+    if args.res:
+        net = ResNetwork(
+            kernel=kernel,
+            embed=args.embed,
+            l0=args.l0,
+            l1=args.l1,
+            l2=args.l2,
+            l3=args.l3,
+            L=args.L,
+            scalar_act=sp,
+            gate_act=rescaled_act.sigmoid
+        )
+    else:
+        net = Network(
+            kernel=kernel,
+            embed=args.embed,
+            l0=args.l0,
+            l1=args.l1,
+            l2=args.l2,
+            l3=args.l3,
+            L=args.L,
+            scalar_act=sp,
+            gate_act=rescaled_act.sigmoid
+        )
 
-ssp = rescaled_act.ShiftedSoftplus(beta=args.beta)
-kernel = create_kernel(
-    cutoff=args.rad_maxr,
-    n_bases=args.rad_nb,
-    n_neurons=args.rad_h,
-    n_layers=args.rad_L,
-    act=ssp
-)
+    ident = torch.nn.Identity()
+    outnet = OutputScalarNetwork(kernel=kernel, previous_Rs=net.Rs[-1], scalar_act=ident)
 
-
-sp = rescaled_act.Softplus(beta=args.beta)
-if args.res:
-    net = ResNetwork(
-        kernel=kernel,
-        embed=args.embed,
-        l0=args.l0,
-        l1=args.l1,
-        l2=args.l2,
-        l3=args.l3,
-        L=args.L,
-        scalar_act=sp,
-        gate_act=rescaled_act.sigmoid
-    )
-else:
-    net = Network(
-        kernel=kernel,
-        embed=args.embed,
-        l0=args.l0,
-        l1=args.l1,
-        l2=args.l2,
-        l3=args.l3,
-        L=args.L,
-        scalar_act=sp,
-        gate_act=rescaled_act.sigmoid
-    )
-
-ident = torch.nn.Identity()
-outnet = OutputScalarNetwork(kernel=kernel, previous_Rs=net.Rs[-1], scalar_act=ident)
-
-output_modules = [
-    spk.atomistic.Atomwise(
-        property=prop,
-        mean=means[prop],
-        stddev=stddevs[prop],
-        atomref=atomrefs[prop],
-        outnet=outnet
-    ) for prop in properties
-]
-
-model = spk.AtomisticModel(net, output_modules)
-
-# build optimizer
-optimizer = torch.optim.Adam(model.parameters(), lr=args.lr)
-
-# hooks
-logging.info("build trainer")
-
+    output_modules = [
+        spk.atomistic.Atomwise(
+            property=prop,
+            mean=means[prop],
+            stddev=stddevs[prop],
+            atomref=atomrefs[prop],
+            outnet=outnet
+        ) for prop in properties
+    ]
+    model = spk.AtomisticModel(net, output_modules)
+    return model
 
 class WallHook(spk.hooks.Hook):
     def __init__(self, wall_max: float):
@@ -179,30 +178,51 @@ class MemoryProfileHook(spk.hooks.Hook):
         )
 
 
-metrics = [spk.train.metrics.MeanAbsoluteError(p, p) for p in properties]
-hooks = [
-    spk.train.CSVHook(log_path=args.model_dir, metrics=metrics),
-    spk.train.ReduceLROnPlateauHook(optimizer, patience=args.reduce_lr_patience),
-    WallHook(args.wall),
-    spk.train.EarlyStoppingHook(patience=args.early_stop_patience),
-    MemoryProfileHook(device),
-]
+def main():
+    # Setup script
+    parser = argparse.ArgumentParser(parents=[train_parser(), qm9_property_selector()])
+    args = parser.parse_args()
+    device = configuration(args)
+    args = create_or_load_directory(args)
+    properties = [vars(QM9)[k] for k, v in vars(args).items() if k in QM9.properties and v]
+    if not properties:
+        raise ValueError("No properties were selected to train on.")
 
-# trainer
-loss = spk.train.build_mse_loss(properties)
-trainer = spk.train.Trainer(
-    args.model_dir,
-    model=model,
-    hooks=hooks,
-    loss_fn=loss,
-    optimizer=optimizer,
-    train_loader=train_loader,
-    validation_loader=val_loader,
-)
+    dataset, split_file, train_loader, val_loader, _ = get_data(args, properties)
+    atomrefs, means, stddevs = get_statistics(dataset, split_file, properties, train_loader)
+    model = create_model(args, atomrefs, means, stddevs, properties)
+    optimizer = torch.optim.Adam(model.parameters(), lr=args.lr)
 
-# run training
-logging.info("training")
-logging.info(f"device: {device}")
-n_epochs = args.epochs if args.epochs else sys.maxsize
-logging.info(f"Max epochs {n_epochs}")
-trainer.train(device=device, n_epochs=n_epochs)
+    # hooks
+    logging.info("build trainer")
+    metrics = [spk.train.metrics.MeanAbsoluteError(p, p) for p in properties]
+    hooks = [
+        spk.train.CSVHook(log_path=args.model_dir, metrics=metrics),
+        spk.train.ReduceLROnPlateauHook(optimizer, patience=args.reduce_lr_patience),
+        WallHook(args.wall),
+        spk.train.EarlyStoppingHook(patience=args.early_stop_patience),
+        MemoryProfileHook(device),
+    ]
+
+    # trainer
+    loss = spk.train.build_mse_loss(properties)
+    trainer = spk.train.Trainer(
+        args.model_dir,
+        model=model,
+        hooks=hooks,
+        loss_fn=loss,
+        optimizer=optimizer,
+        train_loader=train_loader,
+        validation_loader=val_loader,
+    )
+
+    # run training
+    logging.info("training")
+    logging.info(f"device: {device}")
+    n_epochs = args.epochs if args.epochs else sys.maxsize
+    logging.info(f"Max epochs {n_epochs}")
+    trainer.train(device=device, n_epochs=n_epochs)
+
+
+if __name__ == '__main__':
+    main()
