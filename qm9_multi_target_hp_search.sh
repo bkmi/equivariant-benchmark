9e70d4d167eea439491a2216e2ab4389d5d748f8
Ben <benmiller1315@gmail.com>
2020-03-14 11:18:36 +0100

    changed the bessel hp

--- qm9_multi_target_hp_search.sh
+++ qm9_multi_target_hp_search.sh
@@ -57,61 +57,6 @@ case "$1" in
             --Cv
         ;;
 
-    gleichzeitig_mlp)
-        model_dir=${2:-${today}_gleichzeitig_mlp}
-        python qm9_train.py \
-            --model_dir "$model_dir" \
-            --split_file "$split_file" \
-            --db "$db"  \
-            --wall 87120 \
-            --L 5 \
-            --l0 128 \
-            --embed 128 \
-            --rad_h 128 \
-            --res \
-            --mu \
-            --alpha \
-            --homo \
-            --lumo \
-            --gap \
-            --r2 \
-            --zpve \
-            --U0 \
-            --U \
-            --H \
-            --G \
-            --Cv \
-            ---mlp_out
-        ;;
-
-    gleichzeitig_l1_mlp)
-        model_dir=${2:-${today}_gleichzeitig_l1_mlp}
-        python qm9_train.py \
-            --model_dir "$model_dir" \
-            --split_file "$split_file" \
-            --db "$db"  \
-            --wall 87120 \
-            --L 5 \
-            --l0 64 \
-            --l1 21 \
-            --embed 128 \
-            --rad_h 128 \
-            --res \
-            --mu \
-            --alpha \
-            --homo \
-            --lumo \
-            --gap \
-            --r2 \
-            --zpve \
-            --U0 \
-            --U \
-            --H \
-            --G \
-            --Cv \
-            ---mlp_out
-        ;;
-
     gleichzeitig_bessel_mlp)
         model_dir=${2:-${today}_gleichzeitig_bessel_mlp}
         python qm9_train.py \
@@ -119,11 +64,11 @@ case "$1" in
             --split_file "$split_file" \
             --db "$db"  \
             --wall 87120 \
-            --L 5 \
             --l0 128 \
             --embed 128 \
             --rad_h 128 \
             --radial_model bessel \
+            --mlp_out \
             --res \
             --mu \
             --alpha \
@@ -136,8 +81,7 @@ case "$1" in
             --U \
             --H \
             --G \
-            --Cv \
-            ---mlp_out
+            --Cv
         ;;
 
     gleichzeitig_l1_bessel_mlp)
@@ -147,12 +91,14 @@ case "$1" in
             --split_file "$split_file" \
             --db "$db"  \
             --wall 87120 \
-            --L 5 \
             --l0 64 \
             --l1 21 \
             --embed 128 \
             --rad_h 128 \
             --radial_model bessel \
+            --mlp_out \
+            --outnet_l0 64 \
+            --outnet_l1 21 \
             --res \
             --mu \
             --alpha \
@@ -165,8 +111,7 @@ case "$1" in
             --U \
             --H \
             --G \
-            --Cv \
-            ---mlp_out
+            --Cv
         ;;
 
     *)
