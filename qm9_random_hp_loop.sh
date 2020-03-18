99219a81524fa4ba97a1e8702c3cb69f0fdef7cd
Ben <benmiller1315@gmail.com>
2020-03-18 11:23:39 +0100

    always save checkpoints with randsearch

--- qm9_random_hp_loop.sh
+++ qm9_random_hp_loop.sh
@@ -4,6 +4,6 @@ folder="$2"
 counter=0
 while [ $counter -lt $runs ]
 do
-    python qm9_random_hp_search.py "$folder" qm9.db pst.npz
+    python qm9_random_hp_search.py "$folder" qm9.db pst.npz --keep_n_checkpoints 1 --checkpoint_interval 1
     counter=$(( $counter + 1 ))
 done
